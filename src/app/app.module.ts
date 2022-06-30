5116eb6223415ca5aa765e879c721f7939646440
Kejari Palu <kejari.palu.dev@gmail.com>
2022-06-30 13:35:59 +0800

    first init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './templates/navbar/navbar.component';
+import { SidebarComponent } from './templates/sidebar/sidebar.component';
+import { LayoutsComponent } from './templates/layouts/layouts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    LayoutsComponent
   ],
   imports: [
     BrowserModule
