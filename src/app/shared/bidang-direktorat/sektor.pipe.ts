ab4df8e87038bcbea4802614ba69b9c9a7d552a5
Ucup TopekoX <ucup.topekox@gmail.com>
2022-10-21 17:15:00 +0800

    fix sub bid/ sub seksi

--- src/app/shared/bidang-direktorat/sektor.pipe.ts
+++ src/app/shared/bidang-direktorat/sektor.pipe.ts
@@ -13,14 +13,22 @@ export class SektorPipe implements PipeTransform {
             .findIndex(obj => {
                 return obj.namaSektor === value;
         });
-
-        let deskripsi = null;
-        if (args[0] === 'bidang') {
-            const indexBidang = this.bidangDirektoratSektorService.getBidangDirektori()
+        const indexBidang = this.bidangDirektoratSektorService.getBidangDirektori()
                 .findIndex(obj => {
                     return obj.namaBidang === this.bidangDirektoratSektorService.getSektor()[indexSektor].bidangDirektorat;
-            });            
+        });   
+
+        let deskripsi = null;
+        if (args[0] === 'bidang') {         
             deskripsi = this.bidangDirektoratSektorService.getBidangDirektori()[indexBidang].deskripsiBidang;            
+        } else if (args[0] === 'subbid') {
+            // Sub Seksi pada Kejari
+            const bidang = this.bidangDirektoratSektorService.getBidangDirektori()[indexBidang].namaBidang;
+            if (bidang === 'IPOLHANKAM' || bidang === 'SOSBUDMAS' || bidang === 'TIPRODIN') {
+                deskripsi = 'Subseksi A';
+            } else if (bidang === 'EKOKEU' || bidang === 'PAMSTRA') {
+                deskripsi = 'Subseksi B';
+            }
         } else  {
             deskripsi = this.bidangDirektoratSektorService.getSektor()[indexSektor].deskripsiSektor;
         }
