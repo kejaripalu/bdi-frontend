30f69935ad7e614577cf23975caa6a58aacb7250
Ucup TopekoX <ucup.topekox@gmail.com>
2022-11-01 23:32:20 +0800

    fix shorten pipe biy length

--- src/app/shared/shorten.pipe.ts
+++ src/app/shared/shorten.pipe.ts
@@ -7,7 +7,10 @@ import { Pipe, PipeTransform } from "@angular/core";
 export class ShortenPipe implements PipeTransform {
     
     transform(value: any, ...args: any[]) {
-        return value.substr(0, +args[0]) + ' ...';
+        if (value.length > 255) {
+            return value.substr(0, +args[0]) + ' ...';
+        }
+        return value;
     }
 
 }
