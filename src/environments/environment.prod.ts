20083ed5c7e41038f47776722cf02e482bef0fc5
Ucup Topekox <ucup.topekox@gmail.com>
2025-02-11 20:31:18 +0800

    env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://buroncong.timposulabs.com/api/v1',
+  baseUrl: 'http://192.168.1.13:8181/api/v1',
   version: 'v1.0.0-SNAPSHOT'
 };
